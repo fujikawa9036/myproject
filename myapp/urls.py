e78ec198e5ff2c319f4ec95f8349507623d1cc4c
fuji9036 <fujikawa.pitin@gmail.com>
2018-11-05 18:27:35 +0900

    second commit

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.SampleTemplateView.as_view(), name='index'),
+    path('about/',views.About.as_view(), name='about'),
+    path('course/',views.Course.as_view(), name='course'),
+    path('contact/',views.Contact.as_view(), name='contact'),
+    
+
+
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -2,3 +2,12 @@ from django.views.generic import TemplateView
 
 class SampleTemplateView(TemplateView):
     template_name = "myapp/index.html"
+
+class About(TemplateView):
+    template_name = "myapp/about.html"
+
+class Course(TemplateView):
+    template_name = "myapp/course.html"
+
+class Contact(TemplateView):
+    template_name = "myapp/contact.html"
